84bc885101c4cc5aedc0f58503779fabca1fefed
Erl <2102482@ub.edu.ph>
2026-07-07 11:42:12 +0800

    config.py for connecting env file to the connection file

--- config.py
+++ config.py
@@ -18,12 +18,6 @@ DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 DB_NAME = os.getenv("DB_NAME", "supply_management_db")
 
-# --- App ---
-#APP_NAME = "Supply & Inventory Management System"
-#APP_VERSION = "1.0.0"
-#APP_THEME = "flatly"          # ttkbootstrap theme name (try 'darkly', 'cosmo', 'superhero')
-#WINDOW_SIZE = "1200x720"
-
 # --- Business rules ---
 DEFAULT_REORDER_LEVEL = 10    # fallback reorder threshold if item has none set
 
